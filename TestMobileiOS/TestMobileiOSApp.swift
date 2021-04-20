5794f7fc3996de0bab7ac60bf39f742687814bd8
danielmh <danielmartinesh@gmail.com>
2021-04-20 15:22:13 -0500

    project created

---
+++ TestMobileiOS/TestMobileiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMobileiOSApp.swift
+//  TestMobileiOS
+//
+//  Created by Citixen MacBook on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TestMobileiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
